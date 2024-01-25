3c2392dd3548cbdfd555d96aa2440e85eeb14c99
JDieffy <jpd16759@uga.edu>
2024-01-25 13:49:55 -0500

    added comments

--- Python/Blink.py
+++ Python/Blink.py
@@ -1,18 +1,17 @@
-import RPi.GPIO as GPIO
-import time
+import RPi.GPIO as GPIO #sets up the py file to use RPi. 
+import time             #and the time library
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.OUT, initial=GPIO.LOW)
 
-while True:
+while True: #Get settings for squarewave from console
     Frq = int(input("frequency: ")) 
-    T0 = 1/Frq
     duty = int(input("duty percent: "))
     print (Frq, " Hz")
-    Ht = T0*(duty/100)
+    Ht = T0*(duty/100) #take inputted dutycycle and determine time spent high vs low
     Lt = T0*(1-duty/100)
 
-    while True:
+    while True:# squarewave loop
         GPIO.output(26, GPIO.LOW)
         time.sleep(Lt)
         GPIO.output(26, GPIO.HIGH)
--- Python/Blink3.py
+++ Python/Blink3.py
@@ -1,7 +1,4 @@
-import pigpio
+import pigpio #set up pigpio library
 pi=pigpio.pi()
-pi.set_PWM_frequency(26, int(input("frequency: ")))
-pi.set_PWM_dutycycle(26, int(input("duty(0 to 255): ")))
-while True:
-    pass
-pi.set_PWM_dutycycle(26, 0)
+pi.set_PWM_frequency(26, int(input("frequency: ")))# get input for freq
+pi.set_PWM_dutycycle(26, int(input("duty(0 to 255): ")))# and duty
