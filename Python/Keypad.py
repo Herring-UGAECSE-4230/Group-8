1a655690ed8b2acb0e4144f9fc25723ac9c38966
tk51486 <tk51486@uga.edu>
2024-02-29 12:06:36 -0500

    Finished commenting Keypad

--- Python/Keypad.py
+++ Python/Keypad.py
@@ -94,7 +94,7 @@ def runTimer(): #Main loop of the clock. Updates every 60 seconds.
             inTimer = False
             return
         
-def updateClock(): #Sends rising edge to flip flops, updating their value based on state[]
+def updateClock(): #Sends rising edge to all the flip flops, updating their value based on state[]
     for i in range(0, 4):
         displayChar(state[i])
         if(i == 1):
@@ -132,12 +132,12 @@ def compareValue(val): #Checks for valid input for manual clock mode and returns
             if(it == 1 and state[0] == 0 and val == 0): #Turns 00 on the hour digits to 12 since 12:00 AM is midnight.
                state[0] = 1
                state[1] = 2
-            elif(it == 1 and state[0] == 1 and val == 2): #Sets PM light if set to 12 PM.
+            elif(it == 1 and state[0] == 1 and val == 2): #Sets PM light if set to 12 PM (noon).
                am = False
                state[1] = 2
-            elif(it == 1 and ((state[0] == 1 and val >= 3) or (state[0] == 2))):
+            elif(it == 1 and ((state[0] == 1 and val >= 3) or (state[0] == 2))): #If the hour is above 12, turn the PM light on
                 if(state[0] == 2):
-                    if(not(val == 0 or val == 1 or val == 2 or val == 3)):
+                    if(not(val == 0 or val == 1 or val == 2 or val == 3)): #Hour cannot be above 23.
                         return False
                 am = False
                 
@@ -153,54 +153,55 @@ def compareValue(val): #Checks for valid input for manual clock mode and returns
             else:
                 state[it] = val
         return True
-def addValues():
+    
+def addValues(): #Runs when setting the clock manually.
     global inTimer, state, inputKey, off, it
     
-    anchor = time.time()
+    anchor = time.time() #Sets a timer used for flashing.
     updateClock()
-    switch = True
+    switch = True #Variable used for flashing when manually selecting time.
     ledon = False
     while(it <= 3):
-        readKeypad(Xpins[0], char[0:4])
+        readKeypad(Xpins[0], char[0:4]) #Reads through each row, with the four corresponding chars from the array and dictionary.
         readKeypad(Xpins[1], char[4:8])
         readKeypad(Xpins[2], char[8:12])
         readKeypad(Xpins[3], char[12:16])
-        if(off):
+        if(off): #If turned off by a #, display nothing.
             displayChar('NaN')
             for i in range(0, 4):
                 clock(clockPins[i])
         else:
             updateClock()
-            if(time.time() - anchor >= 0.5):
+            if(time.time() - anchor >= 0.5): #Waits for 0.5 seconds to flash on and off.
                 print(switch)
-                anchor = time.time()
+                anchor = time.time() #Resets timer for next loop.
                 if(switch):
-                    state[it] = 8
+                    state[it] = 8 #If the current digit is off, flash an 8.
                     updateClock()
                     switch = False
                 else:
-                    state[it] = 'NaN'
+                    state[it] = 'NaN' #If the current digit is on, turn it off.
                     updateClock()
                     switch = True
-            if(inputKey):
-                if(compareValue(curValue)):
+            if(inputKey): #Runs if an input is pressed.
+                if(compareValue(curValue)): #If the input is valid...
                     if(ledon):
-                        GPIO.output(invalidPin, GPIO.LOW)
+                        GPIO.output(invalidPin, GPIO.LOW) #Turn off the error LED if it's on.
                         ledon = False
                     inputKey = False
                     print(state)
-                    updateClock()
-                    it = it + 1
+                    updateClock() #Display the valid digit and move to the next one.
+                    it = it + 1  
                 else:
                     inputKey = False
-                    GPIO.output(invalidPin, GPIO.HIGH)
+                    GPIO.output(invalidPin, GPIO.HIGH) #If the input is invalid, turn on the error LED and remain on the same digit.
                     ledon = True
     it = 0
     return
-def readKeypad(row, chars):
+def readKeypad(row, chars): #Reads through a single row of the keypad.
     global bCounter, curValue, off, inputKey
 
-    GPIO.output(row, GPIO.HIGH)
+    GPIO.output(row, GPIO.HIGH) #Sends a high value to the input pin of the keypad.
     for j in range(0, 4):
         if(GPIO.input(Ypins[j]) == 1):
             time.sleep(0.5)
@@ -209,7 +210,7 @@ def readKeypad(row, chars):
                 print(bCounter)
                 if(inTimer):
                     curValue = 0
-                    bCounter = bCounter + 1
+                    bCounter = bCounter + 1 #Iterates the B counter
                 else:
                     print('B')
                     inputKey = True
@@ -217,12 +218,12 @@ def readKeypad(row, chars):
             elif(chars[j] == 'A'):
                 if(inTimer):
                     curValue = 0
-                    bCounter = 0
+                    bCounter = 0 #B must be pressed consecutively to return to the start menu.
                 else:
                     print('A')
                     inputKey = True
                     curValue = 'A'
-            elif(chars[j] == '#'):
+            elif(chars[j] == '#'): #Turns off and on the display. Does not change curValue so any logic from the previous input is still applied.
                 if(off):
                     updateClock()
                     off = False
@@ -232,39 +233,40 @@ def readKeypad(row, chars):
             elif(chars[j] == '*'):
                 pass
             else:
-                if(not off):
+                if(not off): #Updates curValue for any other input pressed
                     inputKey = True
                     print(chars[j])
                     curValue = chars[j]
     GPIO.output(row, GPIO.LOW)
-def clock(Pin):
+    
+def clock(Pin): #Sends a rising edge to a specific clock pin.
     GPIO.output(Pin, GPIO.HIGH)
-    #time.sleep(0.25)
     GPIO.output(Pin, GPIO.LOW)
-def startT(): #Runs the beginning menu 
+    
+def startT(): #Runs the start menu.
     global am, curValue, inputKey, state, inTimer
     
     am = True
-    state = [0, 0, 0, 0]
+    state = [0, 0, 0, 0] #Initial value of the SSDs upon startup.
     updateClock()
     while(True):
-        readKeypad(Xpins[0], char[0:4])
+        readKeypad(Xpins[0], char[0:4]) #Only need to read the first 2 rows in the start menu for A and B presses.
         readKeypad(Xpins[1], char[4:8])
         if(inputKey):
             inputKey = False
-            if(curValue == 'A'):
+            if(curValue == 'A'): #Automatic time
                 now = datetime.now()
                 print(now)
-                hour = '{0:02d}'.format(now.hour)
+                hour = '{0:02d}'.format(now.hour) #Uses the suggested formatting to turn the hour into a two-digit string.
                 hour = int(hour)
-                if(hour == 12):
+                if(hour == 12): #Noon is 12 PM.
                     am = False
-                if(hour == 0):
+                if(hour == 0): #00 is midnight, 12 AM.
                     hour = 12
-                if(hour > 12):
+                if(hour > 12): #Hours 13 to 23 are PM.
                     hour = hour - 12
                     am = False
-                state[0] = hour//10
+                state[0] = hour//10  #Assigning the digits of state[] to the proper values.
                 state[1] = hour%10
                 minute = '{0:02d}'.format(now.minute)
                 state[2] = int(minute[0])
@@ -272,9 +274,8 @@ def startT(): #Runs the beginning menu
                 print(state)
                 updateClock()
                 runTimer()
-            if(curValue == 'B'):
+            if(curValue == 'B'): #Runs the required functions for manual time.
                 addValues()
                 runTimer()
 updateClock()
 startT()
-
