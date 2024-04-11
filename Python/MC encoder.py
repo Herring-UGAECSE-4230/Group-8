c306d1cb53ff7c1bbd7e51570420ffe1fff6bf33
tk51486 <tk51486@uga.edu>
2024-04-11 14:36:46 -0400

    commenting decoder

--- Python/MC encoder.py
+++ Python/MC encoder.py
@@ -1,3 +1,4 @@
+#importing any needed libraries
 import RPi.GPIO as GPIO
 import time
 import numpy
@@ -40,6 +41,7 @@ morseChars = {  #character list
     '0':'- - - - -',
     ' ':' '
     }
+#setting up neded GPIOs 
 GPIO.setmode(GPIO.BCM) #Our algorithm is able to be within 1% error range for a length down to about 10 ms.
 SPEAKER_PIN = 2
 LED_PIN = 3
@@ -49,9 +51,10 @@ GPIO.setup(3, GPIO.OUT, initial=GPIO.LOW)
 global length
 length = 100 #length of MC unit in milliseconds
 
+#opens the required input file
 with open ('encoderDemo.txt') as file:
     lines=[line.rstrip() for line in file.readlines()]
-    
+#opens the output file and intializes the outut array to attention    
 outputFile = open('encoder output.txt', 'w')
 output = ['-.-.- | attention']
 morseArr = ['- . - . -']
@@ -61,53 +64,53 @@ for i in lines: #iterates thru each line
     
     it = 0
     for j in words: #iterates thru each word
-        currentWord = "\n"
+        currentWord = "\n"#adds the new line before each word after attention
         if it != 0:
-            currentWord += "       "
+            currentWord += "       "#adds a tab space if the word is the first on the line
         for letter in j:
-            currentWord += morseChars[letter]
-            currentWord += "   "
-        morseArr.append(currentWord.strip())
-        currentWord = currentWord[0:len(currentWord)-2]
-        currentWord += " | " + j
+            currentWord += morseChars[letter]#each letter has its morse code value saved to a temp varaible
+            currentWord += "   "#a blank is added between letter
+        morseArr.append(currentWord.strip())#adds the morse code for each word to the morse arr so it can be iterated through for the led and speaker
+        currentWord = currentWord[0:len(currentWord)-2]#cuts of the added space on the final letter ???
+        currentWord += " | " + j #adds the word and | to teh end of the morse code section for that word
         output.append(currentWord) #end of word
         it += 1
-    morseArr.append("- . -")
-    output.append('\n-.- | over')   
-morseArr.append(". - . - .")
-output.append('\n.-.-. | out') #end of file
+    morseArr.append("- . -")#adds the morse code for over to the array used for led and speaker after each sentence
+    output.append('\n-.- | over')#adds the morse code for over to the output file after each sentence   
+morseArr.append(". - . - .")#adds the morse code for out to the array used for led and speaker at the end
+output.append('\n.-.-. | out') #adds the morse code for out to the output file at the end
 outputFile.writelines(output)
 outputFile.close()
 
-
+#asks and waits for a user to input a freq
 def readFreq():
     global length
     print("Enter duration of a MC unit in ms:")
     length = float(input())
-
+#called on the start of the program
 readFreq()
 
 
-if(length > 50):
-    times = time.perf_counter()
-    it = 0
-    for i in morseArr:
+if(length > 50):#sets the limit for freq that are considered too small based on instructions
+    times = time.perf_counter()#used to calculate the average of the output intervals
+    it = 0#uses an iterator to save how many times there is an output interval
+    for i in morseArr:#iterates through the saved MorseArr to enabl the led and speaker on each dash and dot
         for j in i:
-            test = 1
+            test = 1#defaut value of one interval for a period
             rate = 1
             if(j == " "):
-                test = 0
+                test = 0#changes time to none if it is a blank being output
             if(j == "-"):
-                rate = 3
-            for i in range(0, rate):
+                rate = 3#changes value to 3 times as long if it is a dash being output
+            for i in range(0, rate):#outputs the values to the speaker and led based on the current char 
                 it += 1
                 GPIO.output(2, test)
                 GPIO.output(3, test)
                 time.sleep(length/1000)
-        for i in range(0, 7):
+        for i in range(0, 7):#used to output the long blank after each word
                 it += 1
                 GPIO.output(2, 0)
                 GPIO.output(3, 0)
                 time.sleep(length/1000)
-    timee = time.perf_counter()
-    print((timee - times)/it)
+    timee = time.perf_counter()#sets final time so how long execution took on average can be calculated
+    print((timee - times)/it)#prints the average time
--- Python/MorseDecoder.py
+++ Python/MorseDecoder.py
@@ -1,3 +1,4 @@
+#importing of needed libraries
 import RPi.GPIO as GPIO
 import time
 import numpy
@@ -39,119 +40,105 @@ morseChars = {  #character list
     '- - - . .':'8',
     '- - - - .':'9',
     '- - - - -':'0',
-    '- . -':'over',
     '. - . - .':'out'
     }
 GPIO.setmode(GPIO.BCM) #Our algorithm is able to be within 1% error range for a length down to about 10 ms.
 Input = 17
-#Output = 4 #unused
+
+#setting up of pins used
 SPEAKER_PIN = 2
 LED_PIN = 3
 GPIO.setup(SPEAKER_PIN, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(LED_PIN, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(Input, GPIO.IN)
 #GPIO.setup(Output, GPIO.OUT, initial=GPIO.LOW)
-sum = 0
-#while(True):
-#    GPIO.output(Output, 1)
-#    print(GPIO.input(Input))
+sum = 0#sum use to add all of the time it takes to complete attention
+
 counter = 0
 output = '- . - . -   '
 print("Press down to start")
 ending = False
-while(GPIO.input(Input) == 0):
+while(GPIO.input(Input) == 0):#once key state is switched to pressed the while loop will break and the attention input section will begin
     pass
-anchor = time.time()
+anchor = time.time()#setting the anchor for the current time
 print("Starting")
-prev = GPIO.input(Input)
+prev = GPIO.input(Input)#saves pervious input to see when it changes
 while(counter < 9):
-    time.sleep(0.02)
-    if(prev != GPIO.input(Input)):
-        sum += time.time() - anchor
+    time.sleep(0.02)#small software debounce
+    if(prev != GPIO.input(Input)):#checks if the state has changed from pressed to unpressed or vice versa
+        sum += time.time() - anchor#adds time elapsed to total time
         counter += 1
         prev = GPIO.input(Input)
-        anchor = time.time()
+        anchor = time.time()#sets a new anchor to be used next state change
     print(counter)
-intr = sum/15
+intr = sum/15#calculates the users average interval time based on the attention input
 print(intr)
+#sets up variables to be used in main polling loop
 curr = ""
-anchor = time.time()
+anchor = time.time() 
 prev = GPIO.input(Input)
-currentWord = "" 
-co = 0
-ct = 0
-tabs = ""
+currentWord = ""
+tabs = ""#string that dynamically changes based on if a tab is needed
 tabsBool = False
 ledOn = False
 outputFile = open('decoder output.txt', 'w')
-anchor2 = time.time()
+anchor2 = time.time()#sets a second anchor to be used to print the current word and morse string
 reading = True
-morseWord = ""
-outputFile.write(output + "| " + 'attention\n')
+morseWord = ""#used to save the morse code of the current word
+outputFile.write(output + "| " + 'attention\n')#writes attention to the output file
 while(reading):
-    time.sleep(0.02)
-    if(prev != GPIO.input(Input)):
-        times = time.time() - anchor
-        #print(times)       
+    time.sleep(0.02)#used as a small software debounce
+    if(prev != GPIO.input(Input)):#checks if the state has changed
+        times = time.time() - anchor#calcuates the time that the last high or low took based on the anchor   
         prev = GPIO.input(Input)
-        if(GPIO.input(Input) == 1):
-            GPIO.output(2, 1)
-            GPIO.output(3, 1)
+        if(GPIO.input(Input) == 1):#if the current state is pressed times has tracked the time of the last space
+            GPIO.output(2, 1)#output high to speaker
+            GPIO.output(3, 1)#output high to led
             ledOn = True
-            if(times >= intr*2): #next letter
-                #print('3 space')
-                co = co + 1
-                try:
-                    currentWord += morseChars[curr]
-                    morseWord += curr + "   "
+            if(times >= intr*2): #next letter because time is two times greater than current set interval
+                try:#used in case value is not found in the dictionary
+                    currentWord += morseChars[curr]#add tanslated letter to the current word
+                    morseWord += curr + "   "#adds the current morsecode string to the Morseword for the output
                 except:
-                    currentWord += "?"
+                    currentWord += "?"#current word gets a question mark meaning the whole word is set to question mark if the morse code is not recognized
                 curr = ""
                 anchor = time.time()
-#            elif(time <= 0.05):
-#                pass
-            else:
-                #print('space')
+            else:#case where the time was less than two times the interval space added
                 curr += " "
                 anchor = time.time()
-            if(times >= intr*7): #next word
+            if(times >= intr*7): #next word because time is seven times greater than current set interval
                 print(morseWord)
                 print(currentWord)
-                if(len(morseWord.strip()) != 0):
-                    if(tabsBool):
+                if(len(morseWord.strip()) != 0):#checks if the input is an empty string or only spaces
+                    if(tabsBool):#checks if tab is needed
                         tabs = "    "
                     if(ending):
-                        if(currentWord == 'out'):
+                        if(currentWord == 'k'):#checks if the word is over and ends if lsat word was out
+                            currentWord = 'over'
                             outputFile.write(morseWord + "| " + currentWord)
                             reading = False
-                            break
+                            break#ends the loop
                     ending = False
-                    if(currentWord == 'over'):
-                     ending = True
+                    if(currentWord == 'out'):
+                     ending = True#sets a flag to check if the program will end
                      tabs = ""
-                    if('?' in currentWord):
+                    if('?' in currentWord):#if one ? is found sets the whole word to ? as intended
                         currentWord = '?'
-                    outputFile.write(tabs + morseWord + " | " + currentWord)
-                    curr = ""
+                    outputFile.write(tabs + morseWord + " | " + currentWord)#writes the morse code and the word to the output file
+                    curr = ""#clears all the vars
                     currentWord = ""
                     morseWord = ""
-                    co = 0
-                    ct = ct + 1
                     outputFile.write("\n")
                     tabsBool = True
                 anchor = time.time()
         else:
-            GPIO.output(2, 0)
+            GPIO.output(2, 0)#turns of the led because the key is now up
             GPIO.output(3, 0)
             ledOn= False
             if(times >= intr*2):
-                #print('3 pressed')
-                curr += "-"
-#            elif(time <= 0.05):
-#                pass
+                curr += "-"#adds a dash if time is two times bigger than the interval
             else:
-                #print("pressed")
-                curr += "."
+                curr += "."#adds a period if the interval is less than two times the interval
             anchor = time.time()
     if(ledOn):
         GPIO.output(2, 1)
@@ -160,4 +147,4 @@ while(reading):
         anchor2 = time.time()
         print(curr)
         print(currentWord)
-outputFile.close()
+outputFile.close()#closes the output file to finish writing
